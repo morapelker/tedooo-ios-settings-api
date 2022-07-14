c8bc2e900c285077ef383443f6229ea694302292
morapelker <morapelker@gmail.com>
2022-07-14 15:25:02 +0300

    "v1.0.2"

--- SettingsApi/Classes/AccountDeletionApi.swift
+++ SettingsApi/Classes/AccountDeletionApi.swift
@@ -1,7 +1,19 @@
 import Combine
 
+public enum SettingItem {
+    case lastSeen
+    case liveTranslations
+    case postNotifications
+    case localTime
+}
+
 public protocol AccountDeletionApi {
     
     func deleteAccount() -> AnyPublisher<Any?, Never>
     
+}
+
+public protocol SettingsApi {
+    func updateSettingItem(item: SettingItem, newValue: Bool) -> AnyPublisher<Any?, Error>
+    func updateLanguage(toLanguage: String) -> AnyPublisher<Any?, Error>
 }
